aab74f7ba61d8ebfd4bb6382a914875653e4fcca
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-05-23 19:51:27 +0500

    complite getRandomeTekstureFromTable.js

--- src/components/Storage/TekstureStorageScripts/getRandomTekstureFormTable.js
+++ src/components/Storage/TekstureStorageScripts/getRandomTekstureFormTable.js
@@ -1,14 +1,17 @@
 import { loadDataFromTable } from "../loadDataFromTable";
-import Taste from "../../Model/Taste";
+import Teksture from "../../Model/Teksture";
 
 export default async function getRandomTakstureFromTable(selectedTypes = []) {
     const data = await loadDataFromTable('tekstures');
+    const tekstures = [];
 
-    /**
-     * @todo вставить фильтр
-     */
+    data.forEach(row => {
+        if (selectedTypes.includes(row['type'])) {
+            tekstures.push(new Teksture(row['teksture'], row['type']));
+        }
+    });
 
-    const randomNumber = Math.floor(Math.random() * filteredData.length);
+    const randomNumber = Math.floor(Math.random() * tekstures.length);
 
-    return new Taste(data[randomNumber]);
+    return tekstures[randomNumber];
 }
